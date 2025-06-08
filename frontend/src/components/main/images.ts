62d2a155acef9e507ed29b1ada920b465fdcd37f
Mwandamena <mwandamenakelvin777@gmail.com>
2025-06-08 14:54:35 +0200

    update

--- frontend/src/components/main/images.ts
+++ frontend/src/components/main/images.ts
@@ -8,22 +8,22 @@ export const thumbnails: carouselImages[] = [
   {
     id: 0,
     title: "Image 1",
-    src: "/src/assets/images/image-product-1-thumbnail.jpg",
+    src: "/assets/images/image-product-1-thumbnail.jpg",
   },
   {
     id: 1,
     title: "Image 2",
-    src: "/src/assets/images/image-product-2-thumbnail.jpg",
+    src: "/assets/images/image-product-2-thumbnail.jpg",
   },
   {
     id: 2,
     title: "Image 3",
-    src: "/src/assets/images/image-product-3-thumbnail.jpg",
+    src: "/assets/images/image-product-3-thumbnail.jpg",
   },
   {
     id: 3,
     title: "Image 4",
-    src: "/src/assets/images/image-product-4-thumbnail.jpg",
+    src: "/assets/images/image-product-4-thumbnail.jpg",
   },
 ];
 
@@ -31,21 +31,21 @@ export const carouselImages: carouselImages[] = [
   {
     id: 0,
     title: "Image 1",
-    src: "/src/assets/images/image-product-1.jpg",
+    src: "/assets/images/image-product-1.jpg",
   },
   {
     id: 1,
     title: "Image 2",
-    src: "/src/assets/images/image-product-2.jpg",
+    src: "/assets/images/image-product-2.jpg",
   },
   {
     id: 2,
     title: "Image 3",
-    src: "/src/assets/images/image-product-3.jpg",
+    src: "/assets/images/image-product-3.jpg",
   },
   {
     id: 3,
     title: "Image 4",
-    src: "/src/assets/images/image-product-4.jpg",
+    src: "/assets/images/image-product-4.jpg",
   },
 ];
